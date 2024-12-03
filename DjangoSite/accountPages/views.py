38e05c5639f79ba0f9df503479b5fb21714d945b
Aedan McHale <aedan.mchale@gmail.com>
2024-12-03 17:25:00 +0000

    login problems

--- DjangoSite/accountPages/views.py
+++ DjangoSite/accountPages/views.py
@@ -1,19 +1,25 @@
+import logging
+
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
+
+LOGGER = logging.getLogger("UserLogger")
 
 
 def loginView(request):
     nextPage = request.GET.get("next", "/media")
     context: dict = {"colorMode": request.COOKIES.get("colorMode", "dark")}
+    loggedIn = False
     if request.method == "POST":
         user = authenticate(
             request, username=request.POST["username"], password=request.POST["password"]
         )
         if user:
             login(request, user)
-            # messages.success(request, "Logged in successfully")
-            return redirect(nextPage)
-        else:
-            ...
-            # messages.error(request, "Logged in Fail")
+            LOGGER.info("%s Logged In", user.get_full_name())
+            loggedIn = True
+    
+    if loggedIn or request.user.is_authenticated:
+        return redirect(nextPage)
+    LOGGER.error("USER NOT AUTH")
     return render(request, "accountPages/login.html", context=context)
