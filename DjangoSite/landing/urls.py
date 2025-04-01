9dd6381e29a2418947e8fa9a9e1c95ffd9efab33
Aedan McHale <aedan.mchale@gmail.com>
2025-04-01 09:00:27 +0100

    Added Images and reworked home page

--- DjangoSite/landing/urls.py
+++ DjangoSite/landing/urls.py
@@ -6,11 +6,11 @@ from . import views
 
 urlpatterns = [
     path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("images/favicon.png"))),
-    path("iterlink", views.iterLink, name="iterLink"),
-    path(route="refresh", view=views.refresh, name="Refresh"),
-    path("tools", views.tools, name="tools"),
+    path("iterlink", views.IterLink, name="iterLink"),
+    path(route="refresh", view=views.Refresh, name="Refresh"),
+    path("tools", views.ToolsPage, name="tools"),
     path("log", views.Log, name="log"),
-    path(route="blog", view=views.blogHome, name="blogHome"),
-    path(route="blog/<str:path>", view=views.blogPages, name="blog"),
-    path("", views.index, name="index"),
+    path(route="blog", view=views.BlogHome, name="blogHome"),
+    path(route="blog/<str:path>", view=views.BlogPages, name="blog"),
+    path("", views.Index, name="index"),
 ]
